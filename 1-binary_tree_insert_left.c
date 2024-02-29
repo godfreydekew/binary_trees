c4bfd4a7b43b798bce2339d0c59386a0e584f285
godfreydekew <dekewgodfrey@gmail.com>
2024-02-29 07:25:09 -0800

    fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,11 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *left_node;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *left_node;
 
 	left_node = malloc(sizeof(binary_tree_t));
 	if (left_node == NULL)
