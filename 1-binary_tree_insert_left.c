094ba4554d59e9d1625bade74bd8a6e62a48f507
godfreydekew <dekewgodfrey@gmail.com>
2024-02-29 07:13:46 -0800

    fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,8 +25,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	left_node->right = NULL;
 	if (parent->left != NULL)
 	{
-		parent->left->parent = left_node;
 		left_node->left = parent->left;
+		parent->left->parent = left_node;
 	}
 
 	parent->left = left_node;
