bd922ad893e306543bb68a016e0c791f97545fde
Lagat100 <weldonkiprop1@gmail.com>
2024-03-01 15:37:13 -0800

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
 		return (left > right ? left : right);
 	}
