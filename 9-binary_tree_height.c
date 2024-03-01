62f774dc8fdcc3d0a601c4286769a1f37b576242
Lagat100 <weldonkiprop1@gmail.com>
2024-03-01 14:37:17 -0800

    fix heigt

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,19 @@
  *
  * Return: Height of the tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
 	size_t left, right;
 
-	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	if (tree)
+	{
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (left > right ? left : right);
+		return (left > right ? left : right);
+	}
+
+
+	return (0);
 }
