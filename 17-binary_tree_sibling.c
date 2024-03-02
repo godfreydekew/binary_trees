bcbb0b65ff5749e114a132aba54b4654f76c5708
Lagat100 <weldonkiprop1@gmail.com>
2024-03-01 16:11:17 -0800

    fix sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,8 +14,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent->left == node)
 		return (node->parent->right);
 
-	if (node->parent->right == node)
-		return (node->parent->left);
-
-	return (NULL);
+	return (node->parent->left);
 }
